f95e28549e11b0b9e28a77edd3c7ca3c8dbcebc7
duynguyen2 <dcnguyen233@gmail.com>
2025-10-02 02:19:57 -0700

    random content for now, will fix

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import tailwindcss from '@tailwindcss/vite';
+
+const Navbar = () => {
+    return (
+        <div className='flex justify-between intems-center h-24 text-white '>
+            <h1 className='w-full text-3xl font-bold text-[#00df9a]'>REACT.</h1>
+            <ul className='flex'>
+                <li className='p-4'>Home</li>
+                <li className='p-4'>Company</li>
+                <li className='p-4'>Resources</li>
+                <li className='p-4'>About</li>
+                <li className='p-4'>Contact</li>
+            </ul>
+        </div>
+    );
+};
+
+export default Navbar;
