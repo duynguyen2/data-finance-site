864494fa0c4d65d5f1495cc94b1ada56c54bc49d
duynguyen2 <dcnguyen233@gmail.com>
2025-10-13 15:26:30 -0700

    added teams, work showcase and services showcase, fixed service card component

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,8 @@ import Navbar from './components/Navbar'
 import Hero from './components/Hero';
 import UnknownBy from './components/UnknownBy';
 import Services from './components/Services';
+import OurWork from './components/OurWork';
+import Teams from './components/Teams';
 
 const App = () => {
   const [theme, setTheme] = useState(localStorage.getItem('theme') ? localStorage.getItem('theme') : 'light');
@@ -13,6 +15,8 @@ const App = () => {
       <Hero />
       <UnknownBy />
       <Services />
+      <OurWork />
+      <Teams />
       App
     </div>
   )
